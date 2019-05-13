0f49763e345a69abe5a603ed3e3ea4e4fb423040
tatiana.beskorsova <tatiana.beskorsova@ssa.group>
2019-05-13 03:35:34 +0300

    initial api project

---
+++ Parser.Api/Parser.Api/Controllers/LogLinesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parser.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogLinesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+        
+    }
+}
